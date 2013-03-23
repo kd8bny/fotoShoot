689cb610b8054e702a664b76a9d8e42c51429142
kd8bny <kd8bny@gmail.com>
2013-03-23 17:56:29 -0400

    Updated the v1 R1. Code cleanup, Now utilizes scanner class

--- sShow.py
+++ sShow.py
@@ -1,44 +1,37 @@
-#Digital photo frame
+#sShow
 #Daryl W. Bennett --kd8bny@gmail.com
-#Prupose is to change photos in a slideshow fashion --TODO:Port to RPi
+#Purpose is to show picyures from fotoShoot
 
-#V1 R0
+#V1 R1
 
 #TODO:Close images!
 
-class sShow(QMainWindow, main.Ui_Ui_Form):
-        import os
-        import time
-        import Image
-        import fnmatch
-        import string
+import os
+import time
+import Image
+import scanner
 
-        def scanDir():
-                picList = []
-                for root, dirs, filenames in os.walk(mainDir):
-                        for extension in fileType:
-                                for filename in fnmatch.filter(filenames, extension):
-                                        picList.append(os.path.join(root, filename))
-                return picList
-
-
-        def getPic(pic):
-                fImage = Image.open(pic)
-                return fImage
+class sShow:
+    
+    def __init__(self):
+        self.pause=3
+        fotoScan=scanner.scanner()
+        self.picList=fotoScan.scanDir()
         
-        def showTime():
-                for index, pic in enumerate(picList):
-                        picDis = getPic(pic);
-                        picDis.show()
-                        time.sleep(pause)
-                    
+    def getPic(self,pic):
+            fImage = Image.open(pic)
+            return fImage
         
-        if __name__ == "__main__":
-                mainDir = '.'
-                fileType = ['*.jpg', '*.jpeg', '*.png']
-                pause = 3 #in secs
+    def showTime(self):
+            for index, pic in enumerate(picList):
+                    picDis = getPic(pic);
+                    picDis.show()
+                    time.sleep(pause)
+                           
+if __name__ == "__main__":
+    SShow=sShow()
 
-                picList = scanDir()
+    #Start slide show
+    SShow.showTime(picList)
         
-                #Start slide show
-                showTime()
+    
