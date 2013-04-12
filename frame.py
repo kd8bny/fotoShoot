1e63a01407d11b677d7c1ce66f5fb1aa35ddf75e
kd8bny <kd8bny@gmail.com>
2013-04-12 18:08:50 -0400

    First version of the slideshow window

---
+++ frame.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'framepy.ui'
+#
+# Created: Sun Mar 31 17:31:47 2013
+#      by: PyQt4 UI code generator 4.10
+#
+# WARNING! All changes made in this file will be lost!
+
